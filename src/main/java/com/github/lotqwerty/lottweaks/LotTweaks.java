7ce9a53e59c9205fe7fd6a5ee5620f386a25e207
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2020-11-07 00:24:03 +0900

    Release 1.2.4

--- src/main/java/com/github/lotqwerty/lottweaks/LotTweaks.java
+++ src/main/java/com/github/lotqwerty/lottweaks/LotTweaks.java
@@ -23,7 +23,7 @@ public class LotTweaks {
 
 	public static final String MODID = "lottweaks";
 	public static final String NAME = "LotTweaks";
-	public static final String VERSION = "1.2.3";
+	public static final String VERSION = "1.2.4";
 	public static Logger LOGGER;
 
 	private static final String HAS_BEEN_MOVED = String.format("'BLOCK_GROUPS' config has been moved to '%s'", RotationHelper.BLOCKGROUP_CONFFILE);
