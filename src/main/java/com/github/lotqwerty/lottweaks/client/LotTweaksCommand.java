06501ea4343089e6d25d1584967234cdaaada5af
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-06-14 13:02:29 +0900

    Use RegisterClientCommandsEvent

--- src/main/java/com/github/lotqwerty/lottweaks/client/LotTweaksCommand.java
+++ src/main/java/com/github/lotqwerty/lottweaks/client/LotTweaksCommand.java
@@ -1,15 +1,16 @@
 package com.github.lotqwerty.lottweaks.client;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringJoiner;
 
 import com.github.lotqwerty.lottweaks.LotTweaks;
 import com.github.lotqwerty.lottweaks.client.RotationHelper.Group;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandRuntimeException;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.Item;
@@ -20,58 +21,37 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.ChatFormatting;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.client.event.RegisterClientCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @OnlyIn(Dist.CLIENT)
-public class LotTweaksCommand {
+public class LotTweaksCommand extends LiteralArgumentBuilder<CommandSourceStack> {
 
-	private static final String COMMAND_NAME = '/' + LotTweaks.MODID;
-	private static final String COMMAND_USAGE = String.format("/%s <arg : 'add' or 'reload'>", COMMAND_NAME);
+	protected LotTweaksCommand() {
+		super(LotTweaks.MODID);
+	}
 
 	private static void displayMessage(Component textComponent) {
 		Minecraft.getInstance().gui.handleSystemChat(BuiltinRegistries.CHAT_TYPE.get(ChatType.SYSTEM), textComponent);
 	}
 
 	@SubscribeEvent
-	public void onClientSendChat(final ClientChatEvent event) {
-		String msg = event.getMessage();
-		if (!msg.startsWith(COMMAND_NAME)) {
-			return;
-		}
-		event.setCanceled(true);
-		try {
-			List<String> argsList = new ArrayList<>(Arrays.asList(msg.split(" ")));
-			if (!argsList.get(0).equals(COMMAND_NAME)) {
-				throw new CommandRuntimeException(Component.literal(ChatFormatting.RED + COMMAND_USAGE));
-			}
-			argsList.remove(0);
-			String[] args = argsList.toArray(new String[0]);
-			//
-			if (args.length < 1) {
-				throw new CommandRuntimeException(Component.literal(ChatFormatting.RED + COMMAND_USAGE));
-			}
-			if (args[0].equals("add")) {
-				if (args.length == 2) {
-					if (args[1].equals("1") || args[1].equals("main") || args[1].equals("primary")) {
-						executeAdd(Group.PRIMARY);
-					} else if (args[1].equals("2") || args[1].equals("sub") || args[1].equals("secondary")) {
-						executeAdd(Group.SECONDARY);
-					} else {
-						throw new CommandRuntimeException(Component.literal(ChatFormatting.RED + COMMAND_USAGE));
-					}
-				} else {
-					executeAdd(Group.PRIMARY);
-				}
-			} else if (args[0].equals("reload")) {
-				executeReload();
-			}
-		} catch (CommandRuntimeException e) {
-			displayMessage(e.getComponent());
-		}
+	public void onRegisterCommand(final RegisterClientCommandsEvent event) {
+		LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal(LotTweaks.MODID)
+			.then(Commands.literal("add")
+				.then(Commands.literal("1")
+					.executes(context -> {executeAdd(Group.PRIMARY); return Command.SINGLE_SUCCESS;}))
+				.then(Commands.literal("2")
+					.executes(context -> {executeAdd(Group.SECONDARY); return Command.SINGLE_SUCCESS;}))
+				)
+			.then(Commands.literal("reload")
+				.executes(context -> {executeReload(); return Command.SINGLE_SUCCESS;})
+			)
+		;
+		event.getDispatcher().register(builder);
 	}
-	
+
 	private void executeAdd(Group group) throws CommandRuntimeException {
 		Minecraft mc = Minecraft.getInstance();
 		StringJoiner stringJoiner = new StringJoiner(",");
