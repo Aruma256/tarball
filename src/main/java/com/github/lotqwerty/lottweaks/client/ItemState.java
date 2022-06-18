916781e842e5e2e98dcb533b08884121f32312df
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-06-18 16:37:26 +0900

    Added ItemState class

---
+++ src/main/java/com/github/lotqwerty/lottweaks/client/ItemState.java
@@ -0,0 +1,45 @@
+package com.github.lotqwerty.lottweaks.client;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ItemState {
+
+	protected ItemStack cachedStack;
+
+	public ItemState(Block block) {
+		this(new ItemStack(block));
+	}
+
+	public ItemState(Item item) {
+		this(new ItemStack(item));
+	}
+
+	public ItemState(ItemStack itemStack) {
+		this.cachedStack = itemStack;
+	}
+
+	public ItemStack toItemStack() {
+		return this.cachedStack.copy();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof ItemState)) {
+			return false;
+		}
+		ItemState other = (ItemState)obj;
+		return ItemStack.areItemsEqual(this.cachedStack, other.cachedStack) && ItemStack.areItemStackTagsEqual(this.cachedStack, other.cachedStack);
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 17 * this.cachedStack.getItem().hashCode() + this.cachedStack.getItemDamage();
+		if (this.cachedStack.hasTagCompound()) {
+			hash += this.cachedStack.getTagCompound().hashCode();
+		}
+		return hash;
+	}
+
+}
