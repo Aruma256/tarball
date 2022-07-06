f3d6db2633bc0bfa281400352330cf770334d24d
aruma256 <22253623+aruma256@users.noreply.github.com>
2022-07-06 23:33:12 +0900

    Added test to ItemGroupManager#readGroupFromJsonFile

--- src/test/java/com/github/aruma256/lottweaks/client/ItemGroupManagerTest.java
+++ src/test/java/com/github/aruma256/lottweaks/client/ItemGroupManagerTest.java
@@ -4,11 +4,16 @@ import static com.github.aruma256.lottweaks.TestHelper.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Bootstrap;
@@ -44,6 +49,54 @@ class ItemGroupManagerTest {
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		if (!Bootstrap.isRegistered()) Bootstrap.register();
+	}
+
+	private JsonArray toJsonArray(String str) {
+		return new JsonParser().parse(str).getAsJsonArray();
+	}
+
+	@Test
+	void test_readGroupFromJsonFile() throws Exception {
+		Method method = ItemGroupManager.class.getDeclaredMethod("readGroupFromJsonFile", JsonArray.class);
+		method.setAccessible(true);
+		// accepts an empty array
+		assertEquals(Collections.EMPTY_LIST, method.invoke(null, toJsonArray("[]")));
+		assertTrue(ItemGroupManager.LOG_GROUP_CONFIG.isEmpty());
+		// ignores dictionaries that do not have an id key
+		assertEquals(Arrays.asList(Collections.EMPTY_LIST), method.invoke(null, toJsonArray("[[{'meta':1}]]".replace('\'', '"'))));
+		assertEquals("'id' is missing", ItemGroupManager.LOG_GROUP_CONFIG.poll());
+		assertTrue(ItemGroupManager.LOG_GROUP_CONFIG.isEmpty());
+		// ignores dictionaries with invalid item names
+		assertEquals(Arrays.asList(Collections.EMPTY_LIST), method.invoke(null, toJsonArray("[[{'id':'minecraft:item_that_doesnt_exist'}]]".replace('\'', '"'))));
+		assertEquals("'minecraft:item_that_doesnt_exist' not found", ItemGroupManager.LOG_GROUP_CONFIG.poll());
+		assertTrue(ItemGroupManager.LOG_GROUP_CONFIG.isEmpty());
+		// meta value can be specified
+		assertEquals(Arrays.asList(Arrays.asList(new ItemState(new ItemStack(Blocks.WOOL, 1, 1)))), method.invoke(null, toJsonArray("[[{'id':'minecraft:wool','meta':1}]]".replace('\'', '"'))));
+		assertTrue(ItemGroupManager.LOG_GROUP_CONFIG.isEmpty());
+		// if meta is omitted, it is set to 0
+		assertEquals(Arrays.asList(Arrays.asList(new ItemState(new ItemStack(Blocks.WOOL, 1, 0)))), method.invoke(null, toJsonArray("[[{'id':'minecraft:wool'}]]".replace('\'', '"'))));
+		assertTrue(ItemGroupManager.LOG_GROUP_CONFIG.isEmpty());
+		// nbt can be specified
+		assertEquals(
+			Arrays.asList(Arrays.asList(new ItemState(createNBTstack(new ItemStack(Items.GOLDEN_PICKAXE), "{ench:[{lvl:5s,id:32s}],display:{Name:\"Golden Pickaxe222\"}}")))),
+			method.invoke(null, toJsonArray("[[{'id':'minecraft:golden_pickaxe','nbt':'{ench:[{lvl:5s,id:32s}],display:{Name:\\'Golden Pickaxe222\\'}}'}]]".replace('\'', '"')))
+		);
+		assertTrue(ItemGroupManager.LOG_GROUP_CONFIG.isEmpty());
+		// accepts nested arrays
+		assertEquals(
+			Arrays.asList(
+				Arrays.asList(
+					new ItemState(new ItemStack(Blocks.STONE)),
+					new ItemState(new ItemStack(Items.IRON_HELMET))
+				),
+				Arrays.asList(
+					new ItemState(new ItemStack(Blocks.WOOL, 1, 2)),
+					new ItemState(new ItemStack(Blocks.WOOL, 1, 3))
+				)
+			),
+			method.invoke(null, toJsonArray("[[{'id':'minecraft:stone'},{'id':'minecraft:iron_helmet'}],[{'id':'minecraft:wool','meta':2},{'id':'minecraft:wool','meta':3}]]".replace('\'', '"')))
+		);
+		assertTrue(ItemGroupManager.LOG_GROUP_CONFIG.isEmpty());
 	}
 
 	@Test
